5e65c7f2a7807657e5e503567bc24f096ae9d57d
Cecil Armstrong <cecilgarmstrong@hotmail.com>
2018-02-18 16:55:53 +0000

    Add files via upload

---
+++ args.py
@@ -0,0 +1,14 @@
+﻿import sys
+
+def main():
+    print(sys.argv)
+    for i in range(len(sys.argv)):
+        print("Arg no {} {}".format(i, sys.argv[i]))
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+        with open(fname) as f:
+            print(f.read())
+
+if __name__ == '__main__':
+    main ()
+   
